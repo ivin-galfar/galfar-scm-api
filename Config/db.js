78ae2eec9c5cf617da45fcdefc22b4d2bcca8e6b
ivin-austan <a.ivinaustan@gmail.com>
2026-01-31 13:09:26 +0400

    updated the db

--- Config/db.js
+++ Config/db.js
@@ -8,16 +8,16 @@ const pool = new Pool({
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
-  // ssl: {
-  //   rejectUnauthorized: true, // ensure encryption
-  // },
+  ssl: {
+    rejectUnauthorized: true, // ensure encryption
+  },
 });
 export const connectDB = async () => {
   try {
     const res = await pool.query("SELECT NOW()");
     console.log(
       "✅ PostgreSQL connected successfully! Server time:",
-      res.rows[0].now
+      res.rows[0].now,
     );
   } catch (error) {
     console.error("❌ PostgreSQL connection failed:", error);
