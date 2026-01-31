4b90b6914a6ff514315278f673c5f686ea33094e
ivin-austan <a.ivinaustan@gmail.com>
2026-01-31 10:46:19 +0400

    test:testing cron

--- cron/emailCron.js
+++ cron/emailCron.js
@@ -18,13 +18,17 @@ const pendingStatuses = [
   "pending for ceo",
 ];
 export const cronemails = () => {
-  cron.schedule("36 10 * * *", async () => {
+  cron.schedule("51 10 * * *", async () => {
+    console.log("1.logging");
+
     // Every day at 10:00 AM
     const today = new Date();
     const tomorrow = new Date(today);
     tomorrow.setDate(today.getDate() + 1);
     // Only run if tomorrow is the first day → today is last day of month
     if (tomorrow.getDate() === 1) {
+      console.log("1.entering");
+
       const totalReceipts = await fetchallreceiptslogic(null, statuses);
       const rejectedreceipts = await fetchallreceiptslogic(
         null,
